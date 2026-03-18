d3ce5b376c3a5cfa4b4aa5deaffe18a6bc596f90
Yulia Fig <fihlevska@gmail.com>
2026-03-18 09:51:23 +0100

    update-2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-import { getImagesByQuery } from "./pixabay-api";
+import { getImagesByQuery } from "./js/pixabay-api";
 import { createGallery, clearGallery, showLoader, hideLoader } from "./render-functions";
 
 const form = document.querySelector(".form");
